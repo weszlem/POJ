54297e8c9dee0a173935deaeca14c67e56f36e01
Maciej Gajewski <s7855pjwstk.edu.pl>
2010-03-18 00:26:03 +0100

    Dodano Osobe i dokonczono klase Konto. Commit przed testem.

--- Konto/Konto.java
+++ Konto/Konto.java
@@ -4,6 +4,8 @@ public class Konto
     private int stanKonta;
     private int limitDebetu;
 
+    private static int stopa = 0;
+
     public Konto(Osoba wlasciciel)
     {
 	this.wlasciciel = wlasciciel;
@@ -11,6 +13,53 @@ public class Konto
 	this.limitDebetu = 0;
     }
 
+    public void ustawDebet(int kwota)
+    {
+	if (kwota < 0)
+	    {
+		this.limitDebetu += kwota;
+	    }
+    }
+
     public void wplata(int kwota)
     {
-	
+	this.stanKonta += kwota;
+    }
+
+    public boolean wyplata(int kwota)
+    {
+	if ((this.stanKonta - this.limitDebetu - kwota) > 0)
+	    {
+		this.stanKonta -= kwota;
+		return true;
+	    }
+	else
+	    {
+		return false;
+	    }
+    }
+
+    public boolean przelew(Konto k, int kwota)
+    {
+	if (this.wyplata(kwota))
+	    {
+		k.wplata(kwota);
+		return true;
+	    }
+	else
+	    {
+		System.out.println("Operacja niedozwolona: przelew!");
+		return false;
+	    }
+    }
+
+    public static void ustawStope(int stopa)
+    {
+	Konto.stopa = stopa;
+    }
+
+    public String toString()
+    {
+	return this.wlasciciel + ", stan konta: " + this.stanKonta;
+    }
+}
---
+++ Konto/Osoba.java
@@ -0,0 +1,14 @@
+public class Osoba
+{
+    private String nazwa;
+
+    public Osoba(String nazwa)
+    {
+	this.nazwa = nazwa;
+    }
+
+    public String toString()
+    {
+	return this.nazwa;
+    }
+}
